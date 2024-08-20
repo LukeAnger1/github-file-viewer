5a2f26af04bbefac405a13a4915736a9bc9293a9
Luke Anger <anger.luke2004@gmail.com>
2024-08-20 14:12:56 -0400

    Changed the html to include the module. Changed the js to use the import for web browers rather than node js

--- popup.js
+++ popup.js
@@ -1,18 +1,29 @@
+// import { convertToRawUrl } from './url-converter.js';
+
 document.getElementById("fetchFile").addEventListener("click", async () => {
   const fileUrl = document.getElementById("fileUrl").value;
 
   if (fileUrl) {
     try {
+
+      // alert("before the new file url");
+
+      // Change the url from the top of the web browser to the file url
+      // const newFileUrl = convertToRawUrl(fileUrl);
+
+      // alert("the old url is ", fileUrl);
+      // alert(" the new url is ", newFileUrl);
+
       const response = await fetch(fileUrl);
       const fileContent = await response.text();
       
       if (response.ok) {
         document.getElementById("fileContent").textContent = fileContent;
       } else {
-        alert("Failed to retrieve the file.");
+        alert("Failed to retrieve the file. remove this later");
       }
     } catch (error) {
-      alert("Error fetching the file.");
+      alert("Error fetching the file. remove this later too");
     }
   } else {
     alert("Please enter a GitHub file URL.");

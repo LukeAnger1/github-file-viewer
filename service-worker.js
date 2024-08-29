6f74597c708d37462b24c82add24606afe0b26ee
Luke Anger <anger.luke2004@gmail.com>
2024-08-29 03:24:26 -0400

    creating object sucessfully

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,39 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status === 'complete') {
-        console.log('Tab finished loading:', tab.url);
+        console.log('Tab finished loading:', tab.url, ' with type ', typeof(tab.url));
+
+        console.log('making file retreiver object');
+        fileRetriever = new fileRetriever(tab.url);
+        console.log('after making file retreicer object');
     }
 });
+
+// This class will retreive files based off a given url
+class fileRetriever {
+
+    // Constructor method
+    constructor(rawURL) {
+        this.digest(rawURL);
+    }
+  
+    // This will take a rawURL and set the type of retrieval process that will be used while saving other useful information
+    digest(rawURL) {
+  
+      // Check if the URL is a valid GitHub file URL
+      //if (rawURL.includes('https://github.com/') && rawURL.includes('/blob/')) {
+    if (rawURL.includes('https://github.com/')) {
+  
+        // Replace the base URL and the 'blob' part with the corresponding raw content URL
+        this.digestedURL = rawURL
+          .replace('https://github.com/', 'https://raw.githubusercontent.com/');
+  
+        // This is the type of url to know what needs to be used API wise
+        this.type = "github";
+        
+        return this.digestedURL;
+      } else {
+        throw new Error('Invalid GitHub file URL');
+      }
+    }
+  
+  }

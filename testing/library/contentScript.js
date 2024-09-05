1b2e1cb8edc68122df972975db7d56b9b14260e3
Luke Anger <anger.luke2004@gmail.com>
2024-09-04 20:36:01 -0400

    A maybe working test to import the jquery library

---
+++ testing/library/contentScript.js
@@ -0,0 +1,6 @@
+// This script will run on the matched pages (from manifest.json)
+$(document).ready(function() {
+    console.log("jQuery is working!");
+    $("body").css("background-color", "#f0f0f0");
+  });
+  
